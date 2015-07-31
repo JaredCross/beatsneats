9d483a1e815b22c58fd82d762721d7c5064b8e87
Nick Mattei <nmattei89@gmail.com>
2015-07-31 13:55:18 -0600

    signup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//Home Page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/user/new', function(req,res,next){
+  res.render('users/new')
+})
+
+
+
+
+
 
 module.exports = router;
