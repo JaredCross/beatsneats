8d7d313bfc5427db93ba64fb566a076066be71b1
Jared Cross <cross.jared@gmail.com>
2015-08-04 09:18:29 -0600

    promises changes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var bcrypt = require('bcryptjs')
+var bcrypt = require('bcryptjs');
 var db = require('./../models');
-var unirest = require('unirest');
-var yelp = require('yelp').createClient({
-  consumer_key: process.env.YELP_CONSUMERKEY,
-  consumer_secret: process.env.YELP_CONSUMERSECRET,
-  token: process.env.YELP_TOKEN,
-  token_secret: process.env.YELP_TOKENSECRET
-})
 
-var weatherParser = require('./../lib/server.js')
+var promises = require('./../lib/promise.js');
+var eventParse = require('./../lib/musicEventParse.js');
+
 //Home Page
 router.get('/', function(req, res, next) {
   if (req.session) {
@@ -24,22 +19,22 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/help', function (req,res,next){
-  unirest.get('http://api.songkick.com/api/3.0/search/locations.json?query='+ req.body.city+ '&apikey='+ process.env.SONGKICK_API)
-  .end(function (response) {
-    var cityId = response.body.resultsPage.results.location[0].metroArea.id;
-    unirest.get('http://api.songkick.com/api/3.0/metro_areas/'+ cityId + '/calendar.json?apikey=' + process.env.SONGKICK_API)
-    .end(function (response) {
-      var musicEvents = response.body.resultsPage.results.event
-      yelp.search({term:'food', location: req.body.city}, function (error,data) {
-        var restaurants = data.businesses
-        unirest.post('http://api.openweathermap.org/data/2.5/forecast?q='+req.body.city+',us')
-        .send()
-        .end(function (response) {
-          var weather = weatherParser(response);
-          res.render('index', {musicEvents: musicEvents, restaurants: restaurants, city: req.body.city, state: req.body.state, weather: weather});
-        });
-      })
-    })
+  promises.cityIdPromFunc(req.body.city).then(function (data) {
+    var cityId = data.body.resultsPage.results.location[0].metroArea.id;
+    return Promise.all(
+      [
+        promises.musicEventPromFuncPage1(cityId),
+        promises.musicEventPromFuncPage2(cityId),
+        promises.musicEventPromFuncPage3(cityId),
+        promises.musicEventPromFuncPage4(cityId),
+        promises.yelpPromFunc(req.body.city),
+        promises.weatherPromFunc(req.body.city)
+      ]);
+  }).then(function (results) {
+    var restaurants = results[4].businesses;
+    var weather = results[5];
+    var musicEvent5DaysArr = eventParse(results[0],results[1],results[2],results[3]);
+    res.render('index', {musicEvents: musicEvent5DaysArr, restaurants: restaurants, city: req.body.city, state: req.body.state, weather: weather});
   });
 });
 
@@ -52,9 +47,9 @@ router.post('/login', function (req,res,next){
   db.Users.findOne({email: email}).then(function (user) {
     if (bcrypt.compareSync(password, user.password)) {
       req.session.fullName = user.fullName;
-      res.redirect('/')
+      res.redirect('/');
     }
-  })
+  });
 });
 
 router.get('/logout', function (req,res,next){
@@ -63,8 +58,8 @@ router.get('/logout', function (req,res,next){
 });
 
 router.get('/user/new', function(req,res,next){
-  res.render('users/new')
-})
+  res.render('users/new');
+});
 
 router.post('/user', function(req,res,next){
   var fullName = req.body.fullName;
@@ -73,9 +68,9 @@ router.post('/user', function(req,res,next){
   var hash = bcrypt.hashSync(password, 8);
   db.Users.create({fullName: fullName, email: email, password: hash, destinations: []}).then(function () {
     req.session.fullName = fullName;
-    res.redirect('/')
-  })
-})
+    res.redirect('/');
+  });
+});
 
 
 
