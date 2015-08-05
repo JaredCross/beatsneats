4b6e043b2d0c868f75a2fe56502defdfbee51c09
Roberto Ortega <berto.ort@gmail.com>
2015-08-04 19:04:22 -0600

    login privilages

--- models/users.js
+++ models/users.js
@@ -3,8 +3,6 @@ var mongoose = require('mongoose')
 var userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  favoriteMusic: Array,
-  favoriteFood: Array
 })
 
 var User = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,8 @@ var eventParse = require('./../lib/musicEventParse.js');
 
 //Home Page
 router.get('/', function(req, res, next) {
-  if (req.session) {
-    var fullName = req.session.fullName
-  } else {
-    var fullName = null;
-  }
-      res.render('index', {fullName: fullName,});
+  var email = req.session.email;
+  res.render('index', {email: email});
 });
 
 router.post('/help', function (req,res,next){
@@ -37,15 +33,18 @@ router.post('/help', function (req,res,next){
      .then(function (results) {
      var restaurants = results[4].businesses;
      var weather = results[5];
+     var email = req.session.email;
      var musicEvent5DaysArr = eventParse(results[0],results[1],results[2],results[3]);
-     res.render('index', {musicEvents: musicEvent5DaysArr, restaurants: restaurants, city: req.body.city, weather: weather});
+     res.render('index', {musicEvents: musicEvent5DaysArr, restaurants: restaurants, city: req.body.city, weather: weather, email: email});
    });
    } else {
-     res.render('index', {error: "City isn't valid", city: req.body.city});
+     var email = req.session.email
+     res.render('index', {error: "City isn't valid", city: req.body.city, email: email});
    }
    });
  } else {
-   res.render('index', {error: "City isn't valid", city: req.body.city});
+   var email = req.session.email
+   res.render('index', {error: "City isn't valid", city: req.body.city, email: email});
  }
 });
 
@@ -53,14 +52,13 @@ router.post('/login', function (req,res,next){
   var email = req.body.email;
   var password = req.body.password;
   var error1 = 'All fields must be filled in.'
-  var error2 = 'You credential do not match, please try again.'
-
+  var error2 = 'Your credentials do not match, please try again.'
   if(email.length < 2 || password.length <2){
     res.render('index', {loginError: error1})
   } else {
     db.Users.findOne({email: email}).then(function (user) {
       if (bcrypt.compareSync(password, user.password)) {
-        req.session.fullName = user.fullName;
+        req.session.email = user.email;
         res.redirect('/');
       } else {
         res.render('index', {loginError: error2})
@@ -86,32 +84,17 @@ router.post('/user/new', function(req,res,next){
   var hash = bcrypt.hashSync(password, 8);
   var error1 = 'All fields must be filled in.'
   var error2 = 'Your passwords must match, please try again.'
-
   if (email.length<4 || password.length<4 || confirm.length<4) {
     res.render('index',{error: error1});
   }
   else if (confirm != password){
     res.render('index',{error: error2});
   } else {
-      db.Users.create({ email: email, password: hash, destinations: []}).then(function () {
-        req.session.fullName = fullName;
+      db.Users.create({ email: email, password: hash}).then(function () {
+        req.session.email = email;
         res.redirect('/');
       });
   }
-});
-
-router.post('/user', function(req,res,next){
-  console.log(req.query);
-  var response = {'hello': 'hi'};
-  req.json(response);
-  // var fullName = req.body.fullName;
-  // var email = req.body.email;
-  // var password = req.body.password;
-  // var hash = bcrypt.hashSync(password, 8);
-  // db.Users.create({email: email, password: hash, favoritesMusic: [], favoritesFood: []}).then(function () {
-  //   req.session.fullName = fullName;
-  //   res.redirect('/');
-  // });
 });
 
 module.exports = router;
